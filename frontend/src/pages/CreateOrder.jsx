4a52e16924ee8f261c099d472f1437440992e37c
ssbawaskar08 <shreyasbawaskar0812@gmail.com>
2025-06-24 21:55:35 +0530

    COMMIT BY SHREYAS

--- frontend/src/pages/CreateOrder.jsx
+++ frontend/src/pages/CreateOrder.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const CreateOrder = () => {
     return(
         <>
-            <h1>This is the CreateOrder page</h1>
+            <h1>This is the CreateOrder page.</h1>
         </>
     );
 };
