756b8fd39e8812951a576cf5da43d09514448441
yaxhxv <yashnawgiri45.com>
2025-06-24 15:26:12 +0530

    Add Tailwind CSS and Axios dependencies, update Vite configuration, and enhance App component with new Components folder Add new images and update .gitignore to include .env file.

---
+++ frontend/src/components/buttons/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="bg-[#465FFF] text-white p-2 rounded-md w-80 h-10 flex justify-center items-center">
+        Sign In
+      </button>
+    </div>
+  );
+};
+
+export default Button;
